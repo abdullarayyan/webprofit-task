086f2f1df0ab2b778afde8747e96a82bf2040701
Abdullah Rayyan <arayyan.abd@gmail.com>
2021-03-01 22:02:33 +0200

    validation form using objects

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -9,47 +9,51 @@ const company_website = document.getElementById('company_website')
 const job_title = document.getElementById("job_title")
 const employees = document.getElementById("employees")
 
-//declare regular expression
-const validationName =  /^[a-zA-Z ]{2,30}$/; //regular expression for name & title
-//regular expression for email
-const validationEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const validationPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im; //regular expression for mobile_number
-//regular expression for Url website
-const validationUrl = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-// console.log(form.classList)
-
+const fields_validations = {
+    'first_name'        : /^[a-zA-Z ]{2,30}$/,
+    'last_name'         : /^[a-zA-Z ]{2,30}$/,
+    'job_title'         : /^[a-zA-Z ]{2,30}$/,
+    'company_name'      : /^[a-zA-Z ]{2,30}$/,
+    'employees'         : /disabled selected/, // disabled selected mean: values select option
+    'phone_number'      : /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im, //regular expression for mobile_number
+    'email'             : /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    'company_website'   : new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'), // fragment locator
+}
 // onblur function validation
 form.addEventListener("blur", function (event){
-    event.target.className=('valid')
+    event.target.className=('valid')//initial validation
     //validation fname ,lastnme ,coSize and job_title
      if(event.target.id=='first_name'||
          event.target.id=='last_name'||
          event.target.id=='company_name'||
          event.target.id=='job_title'){
-        if(!validationName.test(event.target.value)){
+        if(!fields_validations.first_name.test(event.target.value)||
+            !fields_validations.last_name.test(event.target.value)||
+            !fields_validations.company_name.test(event.target.value)||
+            !fields_validations.job_title.test(event.target.value)){
             event.target.className=('not-valid')
         }
      }
-     //validation email
+    //validation email
      else if(event.target.id=='email'){
-         if(!validationEmail.test(event.target.value)){
+         if(!fields_validations.email.test(event.target.value)){
              event.target.className=('not-valid')
          }
      }
      //validation phone
      else if(event.target.id=='phone'){
-         if(!validationPhone.test(event.target.value)){
+         if(!fields_validations.phone_number.test(event.target.value)){
              event.target.className=('not-valid')
          }
      }
      //validation company_website
      else if(event.target.id=='company_website'){
-         if(!validationUrl.test(event.target.value)){
+         if(!fields_validations.company_website.test(event.target.value)){
              event.target.className=('not-valid')
          }
      }
@@ -57,7 +61,7 @@ form.addEventListener("blur", function (event){
      else if(event.target.id=='employees'){
          const valSelectEmp = employees.value
          // console.log(valSelectEmp)
-         if(valSelectEmp==='disabled selected'){// disabled selected mean: values select option
+         if(fields_validations.employees.test(event.target.value)){
              event.target.className=('not-valid-select')
          }
      }
